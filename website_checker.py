932d56a2b150b6e7f26bf398c3ff464e75cac26c
thecloudcom <aparajitasaha2024@gmail.com>
2025-10-28 10:06:24 +0530

    Update website_checker.py

--- website_checker.py
+++ website_checker.py
@@ -2,13 +2,16 @@
 import requests
 
 #webste to check 
-website: str = "https://google.com"
+websites = ["https://google.com", "https://www.amazon.com", "https://www.flipkart.com"]
 
-try:
-  response = requests.get(website)
-  if response.status_code == 200:
-    print(f"Success: {website} is online")
-  else:
-      print(f"Warning: {website} returned status code {response.status_code}")
-except requests.exceptions.RequestException:
-  print(f"Error: {website} is Down or cannot be reached")
+for website in websites:
+
+    try:
+        response = requests.get(website)
+        if response.status_code == 200:
+            print(f"Success: {website} is online")
+        else:
+            print(f"Warning: {website} returned status code {response.status_code}")
+
+    except requests.exceptions.RequestException:
+        print(f"Error: {website} is Down or cannot be reached")
